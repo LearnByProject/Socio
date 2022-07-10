4a18579060eef03747ce2493662798ee4fa69516
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-10 18:44:59 +0530

    Postside created

---
+++ src/components/Postside/Postside.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Postshare from '../Postshare/Postshare'
+import './Postside.css'
+const Postside = () => {
+  return (
+    <div className='Postside'>
+      <Postshare/>    </div>
+  )
+}
+
+export default Postside
