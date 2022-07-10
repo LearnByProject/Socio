42697bb0564f48c5d616112b6d02f30fdac59670
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-10 18:41:43 +0530

    Postshare Created

---
+++ src/components/Postshare/Postshare.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Postshare.css'
+import Profileimg from '../../img/profileImg.jpg'
+const Postshare = () => {
+  return (
+    <div className='Postshare'>
+      <img src={Profileimg} alt=''/>
+    </div>
+  )
+}
+
+export default Postshare
