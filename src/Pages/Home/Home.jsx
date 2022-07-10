54941c938ec809a1b89f2d59e72286c373bd04db
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-10 18:45:51 +0530

    Home.jsx Created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Home.css'
 import Profileside from '../../components/Profileside/Profileside'
+import Postside from '../../components/Postside/Postside'
 const Home = () => {
   return (
     <div className='Home'>
         <Profileside/>
-        <div className="postside">Post</div>
+        <Postside/>
         <div className="rightside">RightSide</div>   
     </div>
   )
