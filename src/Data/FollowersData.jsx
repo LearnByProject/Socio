0b97058e78090f8ccf1f6c1f316243910c2596b3
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-10 18:45:28 +0530

    FollowersData jsx Created

--- src/Data/FollowersData.jsx
+++ src/Data/FollowersData.jsx
@@ -4,7 +4,7 @@ import img3 from '../img//img3.png'
 import img4 from '../img//img4.jpg'
 
 export const Followers =[
-    { name : "Virat Kohli" , username : "iamVKohli" , img : img1 },
+    { name : "Virat Kohli" , username : "iamVKohli" , img : img1  },
     { name : "Rohit Sharma" , username : "iamRo" , img : img2 },
     { name : "Cristiano Ronaldo" , username : "CRonaldo" , img : img3 },
     { name : "Anushka Sharma" , username : "anushkaS" , img : img4 }
