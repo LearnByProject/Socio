0b68c208653e06eb5a9990175add2220bce389b4
HARSH RAJENDRA UPADHYE <89975347+Harshu467@users.noreply.github.com>
2022-07-12 23:13:46 +0530

    Updated App js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css"
-import Home from "./Pages/Home/Home";
+import Auth from "./Pages/Home/Auth/Auth";
+// import Home from "./Pages/Home/Home";
+// import Profile from "./Pages/Home/Profile/Profile";
+
 function App() {
   return (
     <div className="App">
       {/* <div className="blur" style={{top:'-18%',right:'0rem'}}></div> */}
       {/* <div className="blur" style={{top:'36%',left:'-8rem'}}></div> */}
-    <Home/>
+    {/* <Home/> */}
+    <Auth/>
+    {/* <Profile/> */}
     </div>
   );
 }
